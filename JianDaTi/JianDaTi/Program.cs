3728fd9fc5397123240270f0f58ce1ead25a9f3c
FankyYao <yaomars@foxmail.com>
2016-11-09 16:56:06 +0800

    测试用例：""

---
+++ JianDaTi/JianDaTi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JianDaTi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestSubString(""));
+        }
+
+        public static int LongestSubString(string s)
+        {
+            if (s.Length <= 0) return 0;
+            Hashtable hs = new Hashtable();
+            char[] c = s.ToArray();
+            int maxLength = 0, count = 0;
+            for (int i = 0;i < c.Length; i++)
+            {
+                if (!hs.Contains(c[i]))
+                {
+                    hs.Add(c[i], true);
+                    count++;
+                }
+                else
+                {
+                    maxLength = Math.Max(maxLength, count);
+                    count = 1;
+                    hs.Clear();
+                    hs.Add(c[i], true);
+                }
+            }
+            return maxLength;
+        }
+    }
+}
